5dfdd867e19c8a1bd052de2bd78b51913212eea9
Francesco Albanese <falbanese96@gmail.com>
2022-06-14 02:02:42 -0400

    Updates to app.js file

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const db = mysql.createConnection(
 
 db.connect((err) => {
     if (err) throw(err);
-    figlet("Employee Manager", function(err, data) {
+    figlet('Employee Manager', function(err, data) {
         if (err) {
-            console.log('Something went wrong...');
+            console.log(`Couldn't create art.`);
             console.dir(err);
             return;
         }
@@ -27,8 +27,57 @@ db.connect((err) => {
     startApp();
 });
 
-async function startApp() {
+function startApp() {
     const primeQ = [{
-        
+        type: 'list',
+        message: `What would you like to do?`,
+        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Update Manager', 'View Emplpoyee by Manager', 'Delete Department', 'Delete Role', 'Delete Employee', 'Exit'],
+        name:'main'
     }]
-}
+
+    inquirer.prompt(primeQ)
+    .then(response => {
+        switch (response.main){
+            case 'View All Employees':
+                viewAll('Employees');
+                break;
+            case 'Add Employee':
+                addNewEmployee();
+                break;
+            case 'Update Employee Role':
+                updateEmployee();
+                break;
+            case 'View All Roles':
+                viewAll('Roles');
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewAll('Departments');
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Update Manager':
+                updateManager();
+                break;
+            case 'View Emplpoyee by Manager': 
+                viewEmployeeByManager();
+                break;
+            case 'Delete Department':
+                deleteDepartment();
+                break;
+            case 'Delete Role':
+                deleteRole();
+                break;
+            case 'Delete Employee':
+                deleteEmployee();
+                break;
+            default:
+                db.end();
+        }       
+    })
+    .catch(err => {console.log(err)});
+};
+    
